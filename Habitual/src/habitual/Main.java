4161c323899132e3124561e7cbaefe1e4c86aefd
Shivash30 <117853204+Shivash30@users.noreply.github.com>
2024-02-20 20:28:19 +0200

    Add files via upload

---
+++ Habitual/src/habitual/Main.java
@@ -0,0 +1,18 @@
+package habitual;
+
+/**
+ *
+ * @author Shivash Jurakan
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainForm form = new MainForm();
+        form.setVisible(true);
+    }
+    
+}
